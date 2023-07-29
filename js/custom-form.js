fa10cb050477fe921d82d37fcf3122f64643fee2
rakeshast <rakesh.bokde@arsenaltech.com>
2023-07-29 19:50:24 +0530

    Working on naming conversation

--- js/custom-form.js
+++ js/custom-form.js
@@ -1,71 +1,62 @@
-jQuery(document).ready(function($) {
-    $('#ast_mobile_detect_formHandler').on('submit', function(e) {
+jQuery(document).ready(function ($) {
+    $('#ast_mobile_detect_formHandler').on('submit', function (e) {
         e.preventDefault();
         var $form = $(this);
         var formData = $form.serialize();
-
-        // Disable the submit button during form submission
         $form.find('button[type="submit"]').prop('disabled', true);
-        
         $.ajax({
             type: 'POST',
-            url: ast_mobile_detect_plugin_ajax_object.ajax_url,
+            url: ast_ajax_object.ajax_url,
             data: formData + '&action=ast_custom_form_submit',
-            success: function(response) {
+            success: function (response) {
                 // Do any additional handling of the response data here
-                if ( response.success === true ) {
-
+                if (response.success === true) {
                     var elementID = 'ast-success';
-
                     // Append the message to the element with the specified ID
-                    $('#' + elementID).append("<p class='ast-success'>" + response.data.message + "</p>" );
-                    
+                    $('#' + elementID).append("<p class='ast-success'>" + response.data.message + "</p>");
+
                     // Clear form inputs and enable the submit button
                     $form[0].reset();
                     // Enable the submit button after form submission
                     $form.find('button[type="submit"]').prop('disabled', false);
                     // Automatically reset the form after 3 seconds
-                    setTimeout(function() {
+                    setTimeout(function () {
                         $form.find('.error-message').remove();
                         $('.ast-success').remove();
                     }, 3000);
-                    
-                }else{
 
-                    // Clear previous error messages
-                    $('.error-message').remove();
-                    // console.log(response.data.errors);
-                    // Display error messages next to corresponding input fields
+                } else {
+					
                     if (response.data.errors) {
-                        $.each(response.data.errors, function(field,message) {
+                        $.each(response.data.errors, function (field, message) {
                             $('#' + field).after('<span class="error-message">' + message + '</span>');
                         });
                     }
 
                     // Enable the submit button after form submission
                     $form.find('button[type="submit"]').prop('disabled', false);
-                    
+
                     // Automatically reset the form after 3 seconds
-                    setTimeout(function() {
+                    setTimeout(function () {
                         $form.find('.error-message').remove();
                     }, 3000);
 
-                }                
+                }
             },
-            error: function(xhr, textStatus, errorThrown) {
+            error: function (xhr, textStatus, errorThrown) {
                 console.log('Error: ' + xhr.responseText);
-                // Enable the submit button after form submission
                 $form.find('button[type="submit"]').prop('disabled', false);
-                // Automatically reset the form after 3 seconds
-                setTimeout(function() {
+                setTimeout(function () {
                     $form.find('.error-message').remove();
                 }, 3000);
             }
         });
+
     });
     // Reset the form and remove error messages when the "Reset" button is clicked
     // $('#ast_mobile_detect_formHandler').on('reset', function() {
-        $('#ast_mobile_detect_formHandler').find('.error-message').remove();
-        $('#ast_mobile_detect_formHandler').find('button[type="submit"]').prop('disabled', false);
+    $('#ast_mobile_detect_formHandler').find('.error-message').remove();
+    $('#ast_mobile_detect_formHandler').find('button[type="submit"]').prop('disabled', false);
     // });
 });
+
--- mobile-device-detector.php
+++ mobile-device-detector.php
@@ -9,7 +9,6 @@ if (!defined('ABSPATH')) {
     die('401 Unauthorized: Access is denied');
 }
 
-
 // Activation hook
 register_activation_hook(__FILE__, 'ast_mobile_detect_activate');
 
@@ -20,13 +19,45 @@ function ast_mobile_detect_activate() {
 
 // Ast Mobile detect wordpress plugin script & stylesheet
 function ast_mobile_detect_plugin_scripts() {
-    wp_enqueue_style('ast_mobile_detect_plugin_css', plugin_dir_url(__FILE__) . 'css/style.css', array(), '1.0');
-    wp_enqueue_script('ast-mobile-detect-plguin_js', plugin_dir_url(__FILE__) . 'js/custom-form.js', array('jquery'), '1.0', true);
-    wp_localize_script('ast-mobile-detect-plguin_js', 'ast_mobile_detect_plugin_ajax_object', array(
-        'ajax_url' => admin_url('admin-ajax.php'),
-    ));
+	
+    $site_key = get_option('custom_recaptcha_site_key');
+    wp_enqueue_style('ast_mobile_detect_plugin-style', plugin_dir_url(__FILE__) . 'css/style.css', array(), time());
+	
+    wp_enqueue_script('ast-mobile-detect-plguin_js', plugin_dir_url(__FILE__) . 'js/custom-form.js', array('jquery'), time(), true);
+	wp_localize_script('ast-mobile-detect-plguin_js', 'ast_ajax_object', array(
+			'ajax_url' => admin_url('admin-ajax.php'),
+	));
+	
+	if (!empty($site_key)) {
+    	wp_enqueue_script('recaptcha', 'https://www.google.com/recaptcha/api.js?render='.esc_attr($site_key), [], time(), true);
+	}   
+	
 }
 add_action('wp_enqueue_scripts', 'ast_mobile_detect_plugin_scripts');
+
+
+function ast_mobile_detect_footer_custom_script() {
+	$site_key = get_option('custom_recaptcha_site_key');
+	if(!empty($site_key)){ ?>
+		<script>
+			jQuery(document).ready(function () {
+				const siteKey = '<?php echo $site_key ?>';
+
+				// Load ReCAPTCHA
+				grecaptcha.ready(function () {
+					// Request the token
+					grecaptcha.execute(siteKey, { action: 'submit' }).then(function (token) {
+						// Append the token to your form
+						$('.g-recaptcha').append('<input type="hidden" name="recaptcha_response" value="' + token + '">');
+					});
+				});
+			});
+		</script>
+	<?php }
+}
+add_action('wp_footer', 'ast_mobile_detect_footer_custom_script');
+
+
 
 // Ast Mobile detect wordpress plugin detect mobile
 function is_mobile_device() {
@@ -52,10 +83,12 @@ function ast_mobile_detect_plugin_shortcode() {
         <form id="ast_mobile_detect_formHandler">
             <div id="ast-errors"></div>
             <div id="ast-success"></div>
-            <!-- Your form fields here -->
+            <!-- Your form fields here -->            
             <p><input type="text" name="name" placeholder="Name" id="name"></p>
             <p><input type="text" name="email" placeholder="Email" id="email"></p>
-            <p><input type="number" name="mobile" placeholder="Mobile" id="mobile"></p>
+            <p><input type="text" name="mobile" placeholder="Mobile" id="mobile"></p>
+			<p><input type="hidden" name="ast_nonce" id="ast-custom-nonce" value="<?php echo wp_create_nonce( 'ast-custom-nonce-action' ); ?>"></p>
+			<div class="g-recaptcha" id="recaptcha"></div>           
             <!-- Add more fields as needed -->
             <button type="submit">Submit</button>
 
@@ -63,7 +96,7 @@ function ast_mobile_detect_plugin_shortcode() {
     <?php //} ?> 
     <?php return ob_get_clean();
 }
-add_shortcode('custom_form', 'ast_mobile_detect_plugin_shortcode');
+add_shortcode('ast_custom_form', 'ast_mobile_detect_plugin_shortcode');
 
 
 // Ast Mobile detect wordpress plugin Ajax request handler
@@ -74,13 +107,22 @@ function ast_custom_form_submit() {
     // Perform form validation and processing here
     // For example:
     global $wpdb;
-
+	
     $errors = array();
     $name = sanitize_text_field($_POST['name']);
     $email = sanitize_email($_POST['email']);
     $mobile = absint($_POST['mobile']);
-
-    // Perform form validation
+   	$response = $_POST['recaptcha_response'];  
+	
+	if ( ! isset( $_POST['ast_nonce'] ) || ! wp_verify_nonce( $_POST['ast_nonce'], 'ast-custom-nonce-action' ) ) {
+        $errors['ast-custom-nonce'] = 'WP_Nonce Invalid!';
+    }  	
+	
+	if (get_recaptcha_response($response)) {
+	}else{
+		$errors['recaptcha'] = 'reCAPTCHA validation failed.';
+	}		
+	
     if (empty($_POST['name'])) {
         $errors['name'] = "Name Field is required."; 
     }
@@ -114,11 +156,11 @@ function ast_custom_form_submit() {
 
     }else{
 
-        // // Register new user when form submited
-        // ast_mobile_detect_woo_user_register($name, $email, $mobile);
+        // Register new user when form submited
+        ast_mobile_detect_woo_user_register($name, $email, $mobile);
 
-        // // Add $attachments as the fifth parameter if you have attachments.
-        // send_download_email($to, $subject, $download_link /*, $attachments*/ );
+        // Add $attachments as the fifth parameter if you have attachments.
+        send_download_email($to, $subject, $download_link /*, $attachments*/ );
 
         //Send response
         $response = array(
@@ -128,6 +170,103 @@ function ast_custom_form_submit() {
     }
 
 }
+
+function get_recaptcha_response($response) {
+	
+
+	$secret_key = get_option('custom_recaptcha_secret_key');
+	
+	if($response){
+		
+		$remote_ip = $_SERVER['REMOTE_ADDR'];
+		// Make a POST request to Google reCAPTCHA API
+		$url = 'https://www.google.com/recaptcha/api/siteverify';
+		$data = array(
+			'secret' => $secret_key,
+			'response' => $response,
+			'remoteip' => $remote_ip
+		);
+		$options = array(
+			'http' => array(
+				'header' => 'Content-type: application/x-www-form-urlencoded',
+				'method' => 'POST',
+				'content' => http_build_query($data),
+			),
+		);
+
+		$context = stream_context_create($options);
+		$result = file_get_contents($url, false, $context);
+		$result_json = json_decode($result);	
+		// Return true or false based on the condition
+		return $result_json->success ? true : false;
+		
+	}else{
+		return true;
+	}
+	
+	
+// 	if( !empty($response) ){	
+// 		$recaptcha_secret = esc_attr($secret_key);
+// 		$recaptcha_response = $response;
+// 		$remote_ip = $_SERVER['REMOTE_ADDR'];
+
+// 		// Make a POST request to Google reCAPTCHA API
+// 		$url = 'https://www.google.com/recaptcha/api/siteverify';
+// 		$data = array(
+// 			'secret' => $recaptcha_secret,
+// 			'response' => $recaptcha_response,
+// 			'remoteip' => $remote_ip
+// 		);
+
+// 		$options = array(
+// 			'http' => array(
+// 				'header' => 'Content-type: application/x-www-form-urlencoded',
+// 				'method' => 'POST',
+// 				'content' => http_build_query($data),
+// 			),
+// 		);
+
+// 		$context = stream_context_create($options);
+// 		$result = file_get_contents($url, false, $context);
+// 		$result_json = json_decode($result);	
+// 		// Return true or false based on the condition
+// 		return $result_json->success ? true : false;
+// 	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 function send_download_email($to, $subject, $download_link /*, $file_path*/ ) {
@@ -207,5 +346,80 @@ function is_valid_mobile_number($mobile_number) {
         return true; // Valid mobile number.
     } else {
         return false; // Invalid mobile number.
+    }
+}
+
+
+
+
+
+// Add admin menu
+add_action('admin_menu', 'custom_recaptcha_plugin_add_admin_menu');
+function custom_recaptcha_plugin_add_admin_menu() {
+    add_options_page('Mobile reCAPTCHA Settings', 'Mobile reCAPTCHA', 'manage_options', 'mobile_recaptcha_settings', 'custom_recaptcha_plugin_settings_page');
+}
+
+// Settings page content
+function custom_recaptcha_plugin_settings_page() {
+    ?>
+    <div class="wrap">
+        <h1>Ast reCAPTCHA Settings</h1>
+        <form method="post" action="options.php">
+            <?php
+                settings_fields('custom_recaptcha_options');
+                do_settings_sections('custom_recaptcha_settings');
+                submit_button();
+            ?>
+        </form>
+    </div>
+    <?php
+}
+
+// Register and add settings
+add_action('admin_init', 'ast_custom_recaptcha_plugin_settings_init');
+function ast_custom_recaptcha_plugin_settings_init() {
+    register_setting('custom_recaptcha_options', 'custom_recaptcha_site_key');
+    register_setting('custom_recaptcha_options', 'custom_recaptcha_secret_key');
+
+    add_settings_section('custom_recaptcha_section', 'reCAPTCHA Settings', 'custom_recaptcha_section_callback', 'custom_recaptcha_settings');
+
+    add_settings_field('custom_recaptcha_site_key', 'Site Key', 'custom_recaptcha_site_key_render', 'custom_recaptcha_settings', 'custom_recaptcha_section');
+    add_settings_field('custom_recaptcha_secret_key', 'Secret Key', 'custom_recaptcha_secret_key_render', 'custom_recaptcha_settings', 'custom_recaptcha_section');
+}
+
+// Section description
+function custom_recaptcha_section_callback() {
+    echo '<p>Please enter your Google reCAPTCHA site key and secret key below:</p>';
+}
+
+// Field render functions
+function custom_recaptcha_site_key_render() {
+    $site_key = get_option('custom_recaptcha_site_key');
+    echo '<input type="text" name="custom_recaptcha_site_key" value="' . esc_attr($site_key) . '" />';
+}
+
+function custom_recaptcha_secret_key_render() {
+    $secret_key = get_option('custom_recaptcha_secret_key');
+    echo '<input type="text" name="custom_recaptcha_secret_key" value="' . esc_attr($secret_key) . '" />';
+}
+
+// Save the keys as options
+add_action('admin_init', 'custom_recaptcha_plugin_save_keys');
+function custom_recaptcha_plugin_save_keys() {
+    if (isset($_POST['custom_recaptcha_site_key'])) {
+        update_option('custom_recaptcha_site_key', sanitize_text_field($_POST['custom_recaptcha_site_key']));
+    }
+
+    if (isset($_POST['custom_recaptcha_secret_key'])) {
+        update_option('custom_recaptcha_secret_key', sanitize_text_field($_POST['custom_recaptcha_secret_key']));
+    }
+}
+
+// Display the reCAPTCHA on your forms
+function custom_recaptcha_display() {
+    $site_key = get_option('custom_recaptcha_site_key');
+
+    if (!empty($site_key)) {
+        echo '<script src="https://www.google.com/recaptcha/api.js?render=' . esc_attr($site_key) . '"></script>';
     }
 }
